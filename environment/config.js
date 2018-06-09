292070626ff3f1e4da046b7800cb6f375bd5a69d
Shashi Kiran S <kiranshashi14@outlook.com>
2018-06-09 00:39:56 -0100

    Adding config.js having app configurations

---
+++ environment/config.js
@@ -0,0 +1,15 @@
+module.exports={
+    mongodb:{
+        //MongoDB 3.6+ Driver URI
+        // mongodbURI:"mongodb://mongodb-stitch-chatroomapp-noexh:subbareddy14@cluster0-shard-00-00-6gsou.mongodb.net:27017,cluster0-shard-00-01-6gsou.mongodb.net:27017,cluster0-shard-00-02-6gsou.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+        //MongoDB <3.6+ Driver URI
+        // mongodbURI:"mongodb+srv://mongodb-stitch-chatroomapp-noexh:subbareddy14@cluster0-6gsou.mongodb.net/test",
+        //mLab URI
+        mongodbURI:"mongodb://shashikiran_sk:subbareddy14@ds133550.mlab.com:33550/skfiresystem",
+        secret:"secret"
+    },
+    google_oauth2:{
+        ClientID:"67619770234-cj1e1m3cqchr3alh97t49mvjl9hnmbv5.apps.googleusercontent.com",
+        ClientSecret:"-XNmZOSQZfi1eowa_tqgeTEP"
+    }
+}
